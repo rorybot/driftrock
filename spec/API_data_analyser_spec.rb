d88b8f784250d3e38421b6a32ae1427966528530
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-02-04 21:58:47 +0000

    GREEN: Produce frequency table of items purchased

--- spec/API_data_analyser_spec.rb
+++ spec/API_data_analyser_spec.rb
@@ -3,11 +3,7 @@ describe DataAnalyser do
     let(:dataAnalyser){DataAnalyser.new}
 
       it 'produces a frequency table of items purchased' do
-        all_purchase_data = File.read("./spec/dataDump.rb")
-        all_purchase_data_eval = Kernel.eval(all_purchase_data).flatten
-        freq_table = Kernel.eval(File.read("./spec/freqTable"))
-        p all_purchase_data_eval
-        expect(dataAnalyser.most_sold(all_purchase_data_eval)).to eq freq_table
+        expect(dataAnalyser.most_sold).to eq "Gorgeous Granite Computer"
       end
 
 end
