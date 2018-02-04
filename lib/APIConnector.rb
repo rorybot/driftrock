132b4fb4aec29244be27cb25ac3bc5bbed536e3f
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-02-04 20:40:06 +0000

    RED: Parse response

--- lib/APIConnector.rb
+++ lib/APIConnector.rb
@@ -1,5 +1,5 @@
 class APIConnector
-  def purchase_response(page)
+  def purchase_response(page, parse)
     uri = 'driftrock-dev-test.herokuapp.com'
     paramsPurchases = '/purchases?per_page=100&page='+page.to_s
 
--- spec/connect_and_request_spec.rb
+++ spec/connect_and_request_spec.rb
@@ -5,4 +5,10 @@ describe APIConnector do
     apiConnector = APIConnector.new
     expect(apiConnector.purchase_response(1)).to eq File.read("purchasesPage1.json")
   end
+
+  it 'parses information received from URI' do
+    purchasesPage1Item1 = {"user_id" => "2K7H-A8SQ-6R5V-8OT5","item" => "Enormous Linen Plate","spend" => "87.16" }
+    apiConnector = APIConnector.new
+    expect(apiConnector.purchase_response(1, 'parse')[0]).to eq purchasesPage1Item1
+  end
 end
