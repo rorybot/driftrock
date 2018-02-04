3b7be8f520da3fc764e5919d6a6adbd5a83dd6fa
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-02-04 20:45:53 +0000

    GREEN: parse response

--- lib/APIConnector.rb
+++ lib/APIConnector.rb
@@ -1,8 +1,9 @@
 class APIConnector
-  def purchase_response(page, parse)
+  def purchase_response(page, parse = false)
     uri = 'driftrock-dev-test.herokuapp.com'
     paramsPurchases = '/purchases?per_page=100&page='+page.to_s
-
     response = Net::HTTP.get(uri, paramsPurchases)
+    return JSON.parse(response)["data"] if parse == 'parse'
+    return response
   end
 end
