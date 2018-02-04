a53a2f98f8ab08602e7dd3e32f2623ddb534fe3b
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-02-04 21:45:23 +0000

    YELLOW[passes with hardcoded purchase data]: Produce frequency table of items purchased

--- spec/API_data_analyser_spec.rb
+++ spec/API_data_analyser_spec.rb
@@ -1,15 +1,13 @@
-require('./lib/APIConnector')
-
-describe APIConnector do
-
-
+require('./lib/dataAnalyser')
+describe DataAnalyser do
     let(:dataAnalyser){DataAnalyser.new}
 
       it 'produces a frequency table of items purchased' do
         all_purchase_data = File.read("./spec/dataDump.rb")
         all_purchase_data_eval = Kernel.eval(all_purchase_data).flatten
         freq_table = Kernel.eval(File.read("./spec/freqTable"))
-        expect(dataAnalyser.mostSold(all_purchases_from_API)).to eq freq_table
+        p all_purchase_data_eval
+        expect(dataAnalyser.most_sold(all_purchase_data_eval)).to eq freq_table
       end
 
 end
