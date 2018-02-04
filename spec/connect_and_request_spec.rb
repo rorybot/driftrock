fb22ce232593d0e7a017ce28aa8c262a9b22e437
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-02-04 21:00:43 +0000

    RED: Dump API to flattened collection for reading

--- spec/connect_and_request_spec.rb
+++ spec/connect_and_request_spec.rb
@@ -1,14 +1,27 @@
 require('./lib/APIConnector')
 
 describe APIConnector do
+
+  let(:apiConnector){APIConnector.new}
+
   it 'connects to a URI and grabs the first page of purchase data' do
-    apiConnector = APIConnector.new
+
     expect(apiConnector.purchase_response(1)).to eq File.read("purchasesPage1.json")
   end
 
   it 'parses information received from URI' do
     purchasesPage1Item1 = {"user_id" => "2K7H-A8SQ-6R5V-8OT5","item" => "Enormous Linen Plate","spend" => "87.16" }
-    apiConnector = APIConnector.new
     expect(apiConnector.purchase_response(1, 'parse')[0]).to eq purchasesPage1Item1
+  end
+
+  it 'scans each page, storing complete, parsed, flattened collection' do
+    #Is this best? Would we have to download an entire database to iterate over it? Could we examine JSON actively?
+
+    #Maybe if frequency table, items are added to it if not already there, or else they are increased by one
+
+    #Would be two step method: grab 1st page, and then a slightly different one for each subsequent page
+
+    expect(apiConnector.dataDump).to eq File.read("./spec/dataDump")
+
   end
 end
