d377a4d2db649bf2c3dc79a2ec23971c52377c97
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-02-04 22:00:02 +0000

    rubocop law enforced

--- lib/APIConnector.rb
+++ lib/APIConnector.rb
@@ -1,10 +1,10 @@
 class APIConnector
   def purchase_response(page, parse = false)
     uri = 'driftrock-dev-test.herokuapp.com'
-    paramsPurchases = '/purchases?per_page=100&page='+page.to_s
+    paramsPurchases = '/purchases?per_page=100&page=' + page.to_s
     response = Net::HTTP.get(uri, paramsPurchases)
-    return JSON.parse(response)["data"] if parse == 'parse'
-    return response
+    return JSON.parse(response)['data'] if parse == 'parse'
+    response
   end
 
   def dataDump
@@ -12,18 +12,12 @@ class APIConnector
     purchases_collection = []
 
     loop do
-
-    purchases_collection << purchase_response(pageNumber, 'parse')
-    pageNumber += 1
-    p purchases_collection.flatten.count
-
-    if purchase_response(pageNumber, 'parse').empty?
-      return purchases_collection.flatten
-    break
+      purchases_collection << purchase_response(pageNumber, 'parse')
+      pageNumber += 1
+      if purchase_response(pageNumber, 'parse').empty?
+        return purchases_collection.flatten
+        break
+      end
     end
-
-    end
-
   end
-
 end
