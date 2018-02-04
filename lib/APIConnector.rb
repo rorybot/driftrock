e9aa2913162b0b151837bf4d2754012d4752ffbe
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-02-04 20:24:19 +0000

    Minimal green on Purchase Response

---
+++ lib/APIConnector.rb
@@ -0,0 +1,5 @@
+class APIConnector
+  def purchase_response(page)
+    File.read("purchases.json")
+  end
+end
--- spec/connect_and_request_spec.rb
+++ spec/connect_and_request_spec.rb
@@ -1,6 +1,8 @@
+require('./lib/APIConnector')
+
 describe APIConnector do
   it 'connects to a URI and grabs the first page of purchase data' do
-    apiConnector = new APIConnector
-    expect(apiConnector.purchase_response(1)).to eq File.read("purchaes.json")
+    apiConnector = APIConnector.new
+    expect(apiConnector.purchase_response(1)).to eq File.read("purchases.json")
   end
 end
